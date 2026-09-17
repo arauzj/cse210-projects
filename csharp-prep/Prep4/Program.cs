0a6393245c0dbb4c789eedaf92b6108c79d0e24d
arauzj <jarauz0508@gmail.com>
2026-09-17 11:56:00 -0400

    This is my prep four project

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Numerics;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        float number = -1;
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = float.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        float total = 0;
+        foreach (float i in numbers)
+            {
+                total = total + i;
+            }
+        Console.WriteLine($"The total is {total}");
+        float average = total / numbers.Count;
+        Console.WriteLine($"The Average is {average}");
+        float num2 = 0;
+        foreach (float num in numbers)
+        {
+            if (num2 < num)
+            {
+                num2 = num;
+            }
+        }
+        Console.WriteLine($"The largest number is {num2}");
     }
 }
