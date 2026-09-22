e1a806c1a1c996ef811ea80dd4b39ac318ed2de4
arauzj <jarauz0508@gmail.com>
2026-09-22 10:48:17 -0400

    These are my finished programs for the week 2 prepare activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2022;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Software Designer";
+        job2._startYear = 2026;
+        job2._endYear = 2028;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Jerry Willfield";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }
